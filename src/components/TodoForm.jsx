23c9c5e5aa4d8fe65b4a861f0ed8540d6061bb14
pnumin <pnumin@pusan.ac.kr>
2025-04-15 10:19:26 +0900

    todolist

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,31 @@
- 
+import TailButton from "../ui/TailButton" 
+import { useRef } from "react"
+export default function TodoForm({addTodo}) {
+  const selRef = useRef() ;
+  const inRef = useRef() ;
 
-export default function TodoForm() {
- 
   const handleClick = (e) => {
-    
+    e.preventDefault() ;
+
+    if (inRef.current.value == "" ) {
+      alert("값을 입력하세요.");
+      inRef.current.focus() ;
+      return ;
+    }
+
+    addTodo(inRef.current.value, selRef.current.value) ;
   }
 
   const handleCancel = (e) => {
-     
+     inRef.current.value = "";
+     inRef.current.focus() ;
+     selRef.current.value = "X" ;
   }
 
   return (
     <form className="w-10/12 grid grid-cols-5 gap-4">
       <select id="sel1"  
+        ref = {selRef}
         className="bg-gray-50 border border-gray-300
                                text-gray-900 text-sm rounded-lg
                                 focus:ring-blue-500 focus:border-blue-500 
@@ -22,6 +35,7 @@ export default function TodoForm() {
       </select>
       <div className="col-span-2 h-full">
         <input type="text" id="txt1" 
+          ref ={inRef}
           className="bg-gray-50 border border-gray-300
                          text-gray-900 text-lg rounded-lg  text-center
                          focus:ring-blue-500 focus:border-blue-500 block w-full  
